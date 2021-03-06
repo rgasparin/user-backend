ba991b884defa07a573d4fcacb121a327e66b79e
robysongasparin <robysongasparin@brq.com>
2021-03-06 19:29:16 -0300

    add cross site permission

--- src/main/java/com/rgasparin/user/UserController.java
+++ src/main/java/com/rgasparin/user/UserController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import io.swagger.annotations.ApiResponses;
 
 //return object saving response http with json or xml
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 //mapping the web request to spring Controller
 @RequestMapping("user")
 public class UserController {
